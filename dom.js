a29e4128950564955b72c9ff11f764b7ba02b8bd
unukuu <ll040299@gmail.com>
2024-06-25 12:17:52 +0800

    changecolor

--- dom.js
+++ dom.js
@@ -1,16 +1,39 @@
-const boxEl = document.getElementsByClassName("box")[0];
+const boxEl = document.getElementsByClassName("box");
 const btnEl = document.getElementsByTagName("button")[0];
-console.log(boxEl);
-console.log(btnEl);
-const rColor = Math.floor(Math.random() * 256);
-const gColor = Math.floor(Math.random() * 256);
-const bColor = Math.floor(Math.random() * 256);
+const addbtn = document.getElementById("addbtn");
+const change = document.getElementById("changebtn");
 
-const colors = "rgb(" + rColor + ", " + gColor + " ," + bColor + ")";
+console.log(boxEl);
+console.log(boxEl.lenght);
+
+function randomcolor() {
+  const rColor = Math.floor(Math.random() * 256);
+  const gColor = Math.floor(Math.random() * 256);
+  const bColor = Math.floor(Math.random() * 256);
+
+  return `rgb(${rColor},${gColor},${bColor})`;
+}
 
 function changebackgroundcolor() {
   console.log("changed");
-  boxEl.style.backgroundColor = colors;
+  for (let i = 0; i < boxEl.length; i++) {
+    boxEl[i].style.backgroundColor = randomcolor();
+    boxEl[i].style.color = randomcolor();
+  }
+  btnEl.style.backgroundColor = randomcolor();
 }
-
 btnEl.addEventListener("click", changebackgroundcolor);
+addbtn.addEventListener("click", function () {
+  const newBox = document.createElement("div");
+  newBox.className = "box";
+
+  const parent = document.getElementsByClassName("container")[0];
+
+  parent.appendChild(newBox);
+});
+//change
+change.addEventListener("click", () => {
+  for (let i = 0; i < boxEl.length; i++) {
+    boxEl[i].classList.toggle("big");
+  }
+});
