850a7e8359a63dc7cd6e3465db1ab101df1f25c0
unukuu <ll040299@gmail.com>
2024-07-01 12:38:47 +0800

    tutorial and todo

--- bootstrap/todo.js
+++ bootstrap/todo.js
@@ -1,1 +1,17 @@
+//DOM elements
+const Submit = document.getElementById("Submit");
 
+//other
+let x = 0;
+let taskArr = [];
+Submit.addEventListener("click", () => {
+  const newTask = {status: x,
+    name: ,
+  }
+  taskArr.push(newTask);
+  console.log(taskArr);
+});
+
+function fvalue() {
+  x = document.getElementById("inputGroupSelect01").value;
+}
